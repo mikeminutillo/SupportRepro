32c1a38cb99255e6b0ddd71e57ff07152c03aacf
Mike Minutillo <mike.minutillo@particular.net>
2021-02-17 11:06:55 +0800

    Remove redundant interface implementation

--- Case62107/Api/ClientGewijzigdEvent.cs
+++ Case62107/Api/ClientGewijzigdEvent.cs
@@ -2,7 +2,7 @@
 {
     using Messages;
 
-    public class ClientGewijzigdEvent : IRegelGewijzigdEvent<ClientRegelData>, IClientGewijzigdEvent
+    public class ClientGewijzigdEvent : IClientGewijzigdEvent
     {
 
     }
